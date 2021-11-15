2c15cd2c7cde349a4b03db50e0b5161c389aa883
Boloom <vseree@enssat.fr>
2021-11-15 12:26:38 +0100

    Test SQL

--- run.py
+++ run.py
@@ -2,4 +2,13 @@
 from appBDD import app
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    run()
+
+def run():
+    os.chdir("./")
+    port = 8080
+    http.server.SimpleHTTPRequestHandler.extensions_map[".wasm"] = "application/wasm"
+    httpd = http.server.HTTPServer(("localhost", port), http.server.SimpleHTTPRequestHandler)
+    pri nt("Running on port %d" % port)
+    httpd.serve_forever()
+
