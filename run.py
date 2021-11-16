46e01f581a4d54a355080a342e960390122e571d
Boloom <vseree@enssat.fr>
2021-11-16 15:01:43 +0100

    màj provisoire

--- run.py
+++ run.py
@@ -2,13 +2,7 @@
 from appBDD import app
 
 if __name__ == "__main__":
-    run()
+    app.run()
 
-def run():
-    os.chdir("./")
-    port = 8080
-    http.server.SimpleHTTPRequestHandler.extensions_map[".wasm"] = "application/wasm"
-    httpd = http.server.HTTPServer(("localhost", port), http.server.SimpleHTTPRequestHandler)
-    pri nt("Running on port %d" % port)
-    httpd.serve_forever()
+
 
