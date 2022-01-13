be82ae05dff35ab9830ea590668cb8dcbe42f3c6
Boloom <vseree@enssat.fr>
2022-01-13 18:52:30 +0100

    Mise à jour TP2

--- appBDD/static/js/tp2/quiz1.js
+++ appBDD/static/js/tp2/quiz1.js
@@ -1,97 +1,113 @@
 // --------------------- GESTION DU QUIZ -------------------------//
 
 (function(){
-    // Functions
-    function buildQuiz(){
-      // variable to store the HTML output
-      const output = [];
-      monQuiz1.forEach((currentQuestion, questionNumber) => {
-          const answers = []; // pour stocker la liste des réponses possibles
-          // On ajoute un bouton 'radio' pour chaque lettre-réponse possible
-          for(letter in currentQuestion.answers){
-            answers.push(
-              `<label>
-                <input type="radio" name="question${questionNumber}" value="${letter}">
-                ${letter} :
-                ${currentQuestion.answers[letter]}
-              </label>`
-            );
-          }
-          output.push( // on ajoute à la sortie la question et les réponses
-            `<div class="slide">
-              <div class="question"> ${currentQuestion.question} </div>
-              <div class="answers"> ${answers.join("")} </div>
-            </div>`
+  // Functions
+  function buildQuiz(){
+    // variable to store the HTML output
+    const output = [];
+    monQuiz2.forEach((currentQuestion, questionNumber) => {
+        const answers = []; // pour stocker la liste des réponses possibles
+        // On ajoute un bouton 'radio' pour chaque lettre-réponse possible
+        for(letter in currentQuestion.answers){
+          answers.push(
+            `<label>
+              <input type="radio" name="question${questionNumber}" value="${letter}">
+              ${letter} :
+              ${currentQuestion.answers[letter]}
+            </label>`
           );
         }
-      );
-      quizContainer.innerHTML = output.join(''); // on combine notre liste de sortie en une string HTML et le mettre sur la page
-    }
-  
-    function showResults(){
-      const answerContainers = quizContainer.querySelectorAll('.answers'); // on rassemble les containers réponses 
-      let numCorrect = 0;
-      monQuiz1.forEach( (currentQuestion, questionNumber) => { // pour chaque question
-        const answerContainer = answerContainers[questionNumber]; // on cherche la réponse sélectionnée
-        const selector = `input[name=question${questionNumber}]:checked`;
-        const userAnswer = (answerContainer.querySelector(selector) || {}).value;
-  
-        if(userAnswer === currentQuestion.correctAnswer){ // si c'est la bonne réponse
-          numCorrect++; // +1 bonne réponse
-          answerContainers[questionNumber].style.color = 'green'; // on colore les réponses en vert 
-        } else {
-          answerContainers[questionNumber].style.color = 'red';  // on colore les réponses en rouge
-        }
-      });
-    }
-  
-    function showSlide(n) {
-      slides[currentSlide].classList.remove('active-slide');
-      slides[n].classList.add('active-slide');
-      currentSlide = n;
-      if(currentSlide === 0){
-        previousButton.style.display = 'none';
-      } else {
-        previousButton.style.display = 'inline-block';
+        output.push( // on ajoute à la sortie la question et les réponses
+          `<div class="slide">
+            <div class="question"> ${currentQuestion.question} </div>
+            <div class="answers"> ${answers.join("")} </div>
+          </div>`
+        );
       }
-      if(currentSlide === slides.length-1){
-        nextButton.style.display = 'none';
-        submitButton.style.display = 'inline-block';
+    );
+    quizContainer.innerHTML = output.join(''); // on combine notre liste de sortie en une string HTML et le mettre sur la page
+  }
+
+  function showResults(){
+    const answerContainers = quizContainer.querySelectorAll('.answers'); // on rassemble les containers réponses 
+    let numCorrect = 0;
+    monQuiz2.forEach( (currentQuestion, questionNumber) => { // pour chaque question
+      const answerContainer = answerContainers[questionNumber]; // on cherche la réponse sélectionnée
+      const selector = `input[name=question${questionNumber}]:checked`;
+      const userAnswer = (answerContainer.querySelector(selector) || {}).value;
+
+      if(userAnswer === currentQuestion.correctAnswer){ // si c'est la bonne réponse
+        numCorrect++; // +1 bonne réponse
+        answerContainers[questionNumber].style.color = 'green'; // on colore les réponses en vert 
       } else {
-        nextButton.style.display = 'inline-block';
-        submitButton.style.display = 'none';
+        answerContainers[questionNumber].style.color = 'red';  // on colore les réponses en rouge
       }
+    });
+    resultsContainer.innerHTML = `${numCorrect} sur ${monQuiz2.length} <br> (tu peux revenir en arrière)`; // on affiche le nb total de bonnes réponses
+  }
+
+  function showSlide(n) {
+    slides[currentSlide].classList.remove('active-slide');
+    slides[n].classList.add('active-slide');
+    currentSlide = n;
+    if(currentSlide === 0){
+      previousButton.style.display = 'none';
+    } else {
+      previousButton.style.display = 'inline-block';
     }
-  
-    function showNextSlide() {
-      showSlide(currentSlide + 1);
+    if(currentSlide === slides.length-1){
+      nextButton.style.display = 'none';
+      submitButton.style.display = 'inline-block';
+    } else {
+      nextButton.style.display = 'inline-block';
+      submitButton.style.display = 'none';
     }
-  
-    function showPreviousSlide() {
-      showSlide(currentSlide - 1);
-    }
-  
-    // Variables
-    const quizContainer = document.getElementById('quiz');
-    const resultsContainer = document.getElementById('results');
-    const submitButton = document.getElementById('submit');
-    const monQuiz1 = [
-      { question: "Question 1",
-        answers: { a: "Réponse 1", b: "Réponse 2", c: "Réponse 3" },
-        correctAnswer: "b" },
-      { question: "Question 2",
-        answers: { a: "Réponse 1", b: "Réponse 2", c: "Réponse 3" },
-        correctAnswer: "b" }
-    ];
-  
-    buildQuiz();
-  
-    const previousButton = document.getElementById("previous");
-    const nextButton = document.getElementById("next");
-    const slides = document.querySelectorAll(".slide");
-    let currentSlide = 0;
-    showSlide(currentSlide);
-    submitButton.addEventListener('click', showResults);
-    previousButton.addEventListener("click", showPreviousSlide);
-    nextButton.addEventListener("click", showNextSlide);
-  })()
+  }
+
+  function showNextSlide() {
+    showSlide(currentSlide + 1);
+  }
+
+  function showPreviousSlide() {
+    showSlide(currentSlide - 1);
+  }
+
+  // Variables
+  const quizContainer = document.getElementById('quiz1');
+  const resultsContainer = document.getElementById('results1');
+  const submitButton = document.getElementById('submit1');
+  const monQuiz2 = [
+    { question: "Tableau à 2 colonnes avec des nombres",
+    answers: { a: "C'est une donnée", b: "C'est une information"},
+    correctAnswer: "a" },
+    { question: "Le nombre '18'",
+    answers: { a: "C'est une donnée", b: "C'est une information"},
+    correctAnswer: "a" },
+    { question: "Tableau avec les températures de chaque ville",
+    answers: { a: "C'est une donnée", b: "C'est une information"},
+    correctAnswer: "b" },
+    { question: "18 ans = âge de la majorité",
+    answers: { a: "C'est une donnée", b: "C'est une information"},
+    correctAnswer: "b" },
+    { question: "Photo de 1000 pixels",
+    answers: { a: "C'est une donnée", b: "C'est une information"},
+    correctAnswer: "a" },
+    { question: "Graphique montrant les entrées d'Avengers chaque semaine",
+    answers: { a: "C'est une donnée", b: "C'est une information"},
+    correctAnswer: "b" },
+    
+  ];
+
+  buildQuiz();
+
+  const previousButton = document.getElementById("previous1");
+  const nextButton = document.getElementById("next1");
+  const slides = document.querySelectorAll(".slide");
+  let currentSlide = 0;
+  showSlide(currentSlide);
+  submitButton.addEventListener('click', showResults);
+  previousButton.addEventListener("click", showPreviousSlide);
+  nextButton.addEventListener("click", showNextSlide);
+})()
+
+
